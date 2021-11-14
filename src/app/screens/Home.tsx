952061e5895f986b52efea79579e2ceeec273e31
Lucas Maidana <lucas_sknr.010@live.com>
2021-11-14 02:58:57 -0300

    Fix de las rutas

--- src/app/screens/Home.tsx
+++ src/app/screens/Home.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { Link, Text, Stack, Container } from "@chakra-ui/react";
+import { Text, Stack, Container } from "@chakra-ui/react";
 import DetailsScreen from "./Details";
 import { Product } from "../../product/types";
 import mock from "../../product/mock";
-
+import { Link } from "react-router-dom";
 interface Props {
   product: Product;
 }
 
 const HomeScreen: React.FC<Props> = ({ product = mock.product }) => {
   return (
-    <Stack height="100vh">
+    <Stack min-height="100vh" height="100%">
       <Container maxWidth="container.xl" paddingX={0} my={4}>
         <Text>{`<HomeScreen />`}</Text>
         <br />
-        <Link href="/product">Ver página de detalle de producto</Link>
-        <DetailsScreen product={product} />
+        <Link to="/product">Ver página de detalle de producto</Link>
+        {/* <DetailsScreen product={product} /> */}
       </Container>
     </Stack>
   );
