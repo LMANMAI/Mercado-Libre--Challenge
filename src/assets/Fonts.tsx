d59adc16cb024c12eac9bbe0d0a293b02c1799d9
Lucas Maidana <lucas_sknr.010@live.com>
2021-11-15 00:08:48 -0300

    fix en las fuentes

--- src/assets/Fonts.tsx
+++ src/assets/Fonts.tsx
@@ -5,31 +5,25 @@ const Fonts = () => (
   <Global
     styles={`@font-face {
           font-family: "Proxima Nova";
-           font-style: normal;
-           font-display: swap;
-          src: url("/src/assets/fonts/proximanova-light.woff2");
+           font-style: normal;          
+          src: url("/fonts/proximanova-light.woff2") format('woff2');
           font-weight: 100;
-           unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+         }
+
+      @font-face {
+          font-family: "Proxima Nova";
+           font-style: normal;          
+          src: url("/fonts/proximanova-regular.woff2") format('woff2');
+          font-weight: normal;          
       }
 
       @font-face {
           font-family: "Proxima Nova";
-           font-style: normal;
-           font-display: swap;
-          src: url("/src/assets/fonts/proximanova-regular.woff2");
-          font-weight: normal;
-           unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-          
+           font-style: normal;          
+          src: url("/fonts/proximanova-semibold.woff2") format('woff2');
+          font-weight: 500;           
       }
-
-      @font-face {
-          font-family: "Proxima Nova";
-           font-style: normal;
-           font-display: swap;
-          src: url("/src/assets/fonts/proximanova-semibold.woff2");
-          font-weight: 500;
-           unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }`}
+      `}
   />
 );
 
